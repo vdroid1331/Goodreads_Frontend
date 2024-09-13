f559140dc5f26109e100b172e228765ffc371365
vdroid1331 <65962958+vdroid1331@users.noreply.github.com>
2024-09-14 00:25:41 +0530

    Basic redux store setup done

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,7 +3,7 @@ import Footer from "Components/Footer/Footer";
 export default function Home() {
   return (
     <>
-      Home component <Footer />{" "}
+      Home component <Footer />
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
 import "./index.css";
 
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import store from "Redux/store";
 
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
+  <Provider store={store}>
     <App />
-  </StrictMode>
+  </Provider>
 );
